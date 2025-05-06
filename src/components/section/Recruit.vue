3b5241db1c39bd071a810857801be45cce68e83f
unko <unko@example.com>
2025-05-06 18:48:49 +0900

    fix: Recruit.vueでRouterLinkのコメントアウトを修正

--- src/components/section/Recruit.vue
+++ src/components/section/Recruit.vue
@@ -14,12 +14,12 @@ import RecruitStepItem from '../parts/RecruitStepItem.vue';
       <div class="mx-3 flex flex-col items-center">
         <div class="flex max-w-3xl flex-col justify-center gap-6">
           <div class="flex flex-col justify-around gap-6 text-center md:flex-row">
-            <RouterLink
+            <!-- <RouterLink
               to="/recruit-info"
               class="flex h-[200px] w-full max-w-[300px] items-center justify-center rounded-lg border-2 border-orange-500 bg-white p-4 text-xl font-bold text-orange-500 shadow-md transition duration-300 ease-in-out hover:bg-orange-500 hover:text-white"
             >
               詳細情報
-            </RouterLink>
+            </RouterLink> -->
           </div>
           <div class="flex flex-col justify-center gap-6 py-10 md:flex-row">
             <RecruitStepItem :number="1" title="応募" description="応募フォームまたはお電話にて応募ください。" />
